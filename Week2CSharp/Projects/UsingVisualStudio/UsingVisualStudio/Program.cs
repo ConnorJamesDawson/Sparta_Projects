13b6225eb9b7054959d9eda22c944ca69049a138
Connor Dawson <Connor.Dawson@hotmail.co.uk>
2023-02-27 16:05:39 +0000

    Finished VisualStudio Task

--- Week2CSharp/Projects/UsingVisualStudio/UsingVisualStudio/Program.cs
+++ Week2CSharp/Projects/UsingVisualStudio/UsingVisualStudio/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace HelloWorld;
+
+public class Program
+{
+    static void Main()
+    {
+        int x = 100;
+        x += 10;
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("i: " + i + " x: " + x);
+            x += i;
+        }
+    }
+}
