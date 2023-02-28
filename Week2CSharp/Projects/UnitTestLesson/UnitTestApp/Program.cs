947052bfb731444d09e0bd1c07f7f80df94b67cb
Connor Dawson <Connor.Dawson@hotmail.co.uk>
2023-02-28 10:54:39 +0000

    Refactored the Work done in Unit Test Lesson

--- Week2CSharp/Projects/UnitTestLesson/UnitTestApp/Program.cs
+++ Week2CSharp/Projects/UnitTestLesson/UnitTestApp/Program.cs
@@ -4,6 +4,28 @@ public class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int userInput = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine(MyMethod(userInput));
+    }
+
+    static string MyMethod(int timeOfDay)
+    {
+        string greeting;
+
+        if (timeOfDay >= 5 && timeOfDay <= 12)
+        {
+            greeting = "Good morning!";
+        }
+        else if (timeOfDay >= 12 && timeOfDay <= 18)
+        {
+            greeting = "Good afternoon!";
+        }
+        else
+        {
+            greeting = "Good evening!";
+        }
+
+        return greeting;
     }
 }
