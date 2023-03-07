abd52cd9a071eb2e7d68ba75ad9ff66e1a40b831
Connor Dawson <Connor.Dawson@hotmail.co.uk>
2023-03-07 14:26:44 +0000

    Finished FizzBuzz Lesson

--- Week3AdvancedUnitTesting&OOP/FizzBuzz/FizzBuzz.Tests/FizzBuzzerShould.cs
+++ Week3AdvancedUnitTesting&OOP/FizzBuzz/FizzBuzz.Tests/FizzBuzzerShould.cs
@@ -6,9 +6,43 @@ namespace FizzBuzz.Tests
     {
 
         [Test]
-        public void FizzBuzz_WheGivenTheNumber1_Output1AsASting()
+        public void FizzBuzz_WhenGivenTheNumber1_Output1AsASting()
         {
             Assert.That(FizzBuzzer.FizzBuzz(1), Is.EqualTo("1"));
+        }
+
+        [TestCase(2, "2")]
+        [TestCase(4, "4")]
+        public void FizzBuzz_WhenGivenANumber_NotDivisableBy3or5_ReturnItAsASting(int number, string expected)
+        {
+            Assert.That(FizzBuzzer.FizzBuzz(number), Is.EqualTo(expected));
+        }
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(12)]
+        public void FizzBuzz_WhenGivenANumber_DivisableBy3_ReturnFizzAsASting(int number)
+        {
+            Assert.That(FizzBuzzer.FizzBuzz(number), Is.EqualTo("Fizz"));
+        }
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void FizzBuzz_WhenGivenANumber_DivisableBy5_ReturnBuzzAsASting(int number)
+        {
+            Assert.That(FizzBuzzer.FizzBuzz(number), Is.EqualTo("Buzz"));
+        }
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(60)]
+        public void FizzBuzz_WhenGivenANumber_DivisableBy5and5_ReturnFizzBuzzAsASting(int number)
+        {
+            Assert.That(FizzBuzzer.FizzBuzz(number), Is.EqualTo("FizzBuzz"));
+        }
+
+        [TestCase(1,"1")]
+        public void FizzBuzz_WhenGivenAnEndNumber_IfDivisableBy5and5_ReturnFizzBuzz_ElseReturnNumberAsString(int number, string expected)
+        {
+            Assert.That(FizzBuzzer.FizzBuzz(number), Is.EqualTo(expected));
         }
     }
 }
--- Week3AdvancedUnitTesting&OOP/FizzBuzz/FizzBuzz.app/FizzBuzzer.cs
+++ Week3AdvancedUnitTesting&OOP/FizzBuzz/FizzBuzz.app/FizzBuzzer.cs
@@ -3,9 +3,13 @@
 public class FizzBuzzer
 {
 
-    public static string FizzBuzz(int stringInput)
+    public static string FizzBuzz(int input)
     {
-        return "1";
+        if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
+        else if (input % 3 == 0) return "Fizz";
+        else if (input % 5 == 0) return "Buzz";
+
+        return input.ToString();
     }
 
 }
